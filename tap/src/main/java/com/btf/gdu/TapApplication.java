d50f658c9c56ec66566547901400cc8a3a98a6e5
akma517 <akma517@naver.com>
2021-12-03 12:00:30 +0900

    initial project

---
+++ tap/src/main/java/com/btf/gdu/TapApplication.java
@@ -0,0 +1,13 @@
+package com.btf.gdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapApplication.class, args);
+	}
+
+}
