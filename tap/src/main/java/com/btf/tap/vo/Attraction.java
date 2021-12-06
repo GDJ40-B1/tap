7983cf2d3446f6e9e8cdf3b9820be2eb4303f0ac
kiwon2 <45128285+kiwon2@users.noreply.github.com>
2021-12-06 14:52:48 +0900

    [유기원] pom 설정 추가, attraction 관련 vo 추가

---
+++ tap/src/main/java/com/btf/tap/vo/Attraction.java
@@ -0,0 +1,15 @@
+package com.btf.tap.vo;
+
+import lombok.Data;
+
+@Data
+public class Attraction {
+	private int attractionId;
+	private int detailAddressId;
+	private String attractionCategory;
+	private String attractionName;
+	private String attractionPhoneNumber;
+	private String attractionContent;
+	private String createDate;
+	private String updateDate;
+}
---
+++ tap/src/main/java/com/btf/tap/vo/AttractionCategory.java
@@ -0,0 +1,10 @@
+package com.btf.tap.vo;
+
+import lombok.Data;
+
+@Data
+public class AttractionCategory {
+	private String attractionCategory;
+	private String createDate;
+	private String updateDate;	
+}
