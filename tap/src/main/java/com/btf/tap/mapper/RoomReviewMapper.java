2d8f30a67c90f0ab31aabd7ceccf3e7e18efc95c
gustn <gustn@DESKTOP-BOL0KJS>
2021-12-30 17:29:07 +0900

    [오현수] 숙소 상세보기 페이지에서 숙소후기 평균평점 기능구현 1. mapper에서 숙소별 숙소후기 총 평점을 구하는 쿼리,메소드 구현 2. service에서 숙소별 숙소후기 평균평점 구현 3. roomOne.jsp에서 평균평점 적용

--- tap/src/main/java/com/btf/tap/mapper/RoomReviewMapper.java
+++ tap/src/main/java/com/btf/tap/mapper/RoomReviewMapper.java
@@ -12,11 +12,12 @@ import com.btf.tap.vo.RoomReviewComment;
 @Mapper
 public interface RoomReviewMapper {
 	List<RoomReview> selectRoomReviewList(Map<String,Object> page); // 해당숙소의 전체 후기 조회하기
-	int totalRoomReivewCount(int roomId); // 전체 후기 수
+	int totalRoomReivewCount(int roomId); // 해당숙소의 전체 후기 수
 	int insertRoomReview(RoomReview roomReview); // 숙소후기 작성하기
 	int deleteRoomReview(int roomReviewId); // 숙소후기 삭제하기
 	int insertRoomReviewComment(RoomReviewComment roomReviewComment); // 숙소후기 답변 작성하기
 	int deleteRoomReviewComment(int roomReviewId); // 숙소후기 답변 삭제하기
+	double totalRoomReviewScore(int roomId); // 숙소별 평점 총합 
 	
 	// *** 특정 회원을 기준 ***
 	List<RoomReview> selectMemberRoomReviewList(Map<String,Object> page); // 전체 숙소후기 목록 조회하기
